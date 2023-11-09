1f2bc87960f0100b11920131a5b91ae8c21786bf
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2023-11-09 20:03:50 +0530

    subtests and helper functions added , readme included

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+//It's worth thinking about creating constants to capture the meaning of values and to even aid performance.
+const greetingPrefix = "Hello , "
+
 func Hello(name string) string {
-	return "Hello , " + name
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix + name
 }
 
 func main(){
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World"))
 }
